839e60f7986bc706bf208cd4e7ebbef6264f5278
mohammad amin <mmdamin_khajehkoolaki256@protonmail.com>
2024-02-23 15:54:41 +0330

    initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# tstick.py
+A simple no-nonsense script that converts images to telegram stickers.
+input the images as parameters(globbing is accepted as well) and viola your stickers are ready!
+the default output directory is ```"~/Pictures/tstickers"```
+However you can override it with the ```-o``` flag.
+
+# dependencies
+- [python-filetype](https://github.com/h2non/filetype.py)
+- [pillow](https://pypi.org/project/pillow/)
+
+# tip:
+send the output images to t.me/stickers to create your own sticker pack.
---
+++ tstick.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+from PIL import Image
+import argparse
+from os import makedirs, path, getenv
+import filetype
+
+home = getenv("HOME")
+
+parser = argparse.ArgumentParser(
+    description="Convert any picture to a telegram sticker on the fly."
+)
+
+parser.add_argument(
+    "-o",
+    "--output",
+    metavar="",
+    default=f"{home}/Pictures/tstickers",
+    help="specify the output directory(default is ~/Pictures/tstickers).",
+)
+
+parser.add_argument(
+    "images",
+    metavar="",
+    nargs="+",
+)
+
+args = parser.parse_args()
+
+
+def requirements():
+    if not path.exists(args.output):
+        makedirs(args.output, exist_ok=True)
+
+
+def check(file: str) -> bool:
+    is_image = filetype.is_image(file)
+    if is_image:
+        return True
+    else:
+        return False
+
+
+def convert(file: str, file_name: str) -> None:
+    extension = file_name.split(".")[1]
+    output_file = file_name.removesuffix(extension)
+    img = Image.open(file)
+    img.thumbnail((512, 512))
+    img.save(f"{args.output}/{output_file}webp")
+
+
+def main():
+    for image in args.images:
+        image_file = image.split("/")[-1]
+        if not check(image):
+            continue
+        requirements()
+        convert(image, image_file)
+
+
+if __name__ == "__main__":
+    main()
