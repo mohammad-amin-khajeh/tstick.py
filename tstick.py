304058b020d9684216a5cad3fe77c47c2dbc2c66
mohammad amin <mmdamin_khajehkoolaki256@protonmail.com>
2024-02-27 23:23:32 +0330

    add support for video stickers.
    currently only supports wide videos.

--- tstick.py
+++ tstick.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from subprocess import call
 from PIL import Image
 import argparse
 from os import makedirs, path, getenv
@@ -21,7 +22,23 @@ parser.add_argument(
 )
 
 parser.add_argument(
-    "images",
+    "-s",
+    "--start",
+    metavar="",
+    default="00:00:00",
+    help="specify the starting time of video stickers('only works on videos').",
+)
+
+parser.add_argument(
+    "-d",
+    "--duration",
+    metavar="",
+    default="00:00:02.950",
+    help="specify the duration of the video sticker.",
+)
+
+parser.add_argument(
+    "files",
     metavar="",
     nargs="+",
 )
@@ -29,35 +46,51 @@ parser.add_argument(
 args = parser.parse_args()
 
 
-def requirements():
-    if not path.exists(args.output):
-        makedirs(args.output, exist_ok=True)
-
-
 def check(file: str) -> bool:
-    is_image = filetype.is_image(file)
-    if is_image:
+    is_valid = filetype.is_image(file) or filetype.is_video(file)
+    if is_valid:
         return True
     else:
         return False
 
 
-def convert(image: str, file_name: str) -> None:
-    extension = file_name.split(".")[-1]
-    output_file = file_name.removesuffix(extension)
-    img = Image.open(image)
-    img.thumbnail((512, 512))
-    img.save(f"{args.output}/{output_file}webp")
-    print(f"{file_name} has been converted")
+def requirements():
+    if not path.exists(args.output):
+        makedirs(args.output, exist_ok=True)
+
+
+def determine_file_type(file: str) -> str:
+    if filetype.is_image(file):
+        return "image"
+    elif filetype.is_video(file):
+        return "video"
+    else:
+        raise ValueError
+
+
+def convert(file_type: str, file: str, base_name: str) -> None:
+    extension = base_name.split(".")[-1]
+    output_file = base_name.removesuffix(extension)
+    if file_type == "image":
+        img = Image.open(file)
+        img.thumbnail((512, 512))
+        img.save(f"{args.output}/{output_file}webp")
+        print(f"{base_name} has been converted")
+    else:
+        call(
+            f"ffmpeg -ss {args.start} -t {args.duration} -an -i '{file}' -c:v libvpx-vp9 -b:v 500k -vf scale=512:-2 -r 30 '{args.output}/{output_file}webm'",
+            shell=True,
+        )
 
 
 def main():
-    for image in args.images:
-        image_file = image.split("/")[-1]
-        if not check(image):
+    for file in args.files:
+        base_name = file.split("/")[-1]
+        if not check(file):
             continue
         requirements()
-        convert(image, image_file)
+        file_type = determine_file_type(file)
+        convert(file_type, file, base_name)
 
 
 if __name__ == "__main__":
